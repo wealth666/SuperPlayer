3fdd604c3dc31f0951f9e6faf6e54325a222b322
seven <8536397+seven001@user.noreply.gitee.com>
2021-12-30 20:24:05 +0800

    feat:修改空引用问题

--- Flutter/lib/Core/superplayer_plugin.dart
+++ Flutter/lib/Core/superplayer_plugin.dart
@@ -9,19 +9,19 @@ class SuperPlayerPlugin {
     return version;
   }
 
-  static Future<int> createLivePlayer() async{
+  static Future<int?> createLivePlayer() async{
     return await _channel.invokeMethod('createLivePlayer');
   }
 
-  static Future<int> createVodPlayer() async{
+  static Future<int?> createVodPlayer() async{
     return await _channel.invokeMethod('createVodPlayer');
   }
 
-  static Future<int> setConsoleEnabled(bool enabled) async{
+  static Future<int?> setConsoleEnabled(bool enabled) async{
     return await _channel.invokeMethod('setConsoleEnabled',{"enabled":enabled});
   }
 
-  static Future<int> releasePlayer(int playerId) async{
+  static Future<int?> releasePlayer(int playerId) async{
     return await _channel.invokeMethod('releasePlayer',{"playerId":playerId});
   }
 
